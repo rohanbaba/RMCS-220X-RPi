9316e0afee3e08bff4c001a3d6fad61ffa2d88ce
rohanbaba <rohanpchandratre@gmail.com>
2020-04-21 02:57:14 +0530

    Debugging

--- Examples/two_motor_oscilations.py
+++ Examples/two_motor_oscilations.py
@@ -11,21 +11,46 @@ def read():
     return ser.readline().decode()
 
 
-# Read Position
-ser.write("0,P\r\n".encode())
-print("Max speed of Motor 0 (before turning):", read())
-ser.write("1,P\r\n".encode())
-print("Max speed of Motor 1 (before turning):", read())
+# Max Speed to 250
+ser.write("0,M,240\r\n".encode())
+print("Max Speed of Motor 0 :", read())
+ser.write("1,M,240\r\n".encode())
+print("Max Speed of of Motor 1 :", read())
 
+# Current Position to 0
+ser.write("0,P,0\r\n".encode())
+print(read())
+ser.write("1,P,0\r\n".encode())
+print(read())
 
-# Go To 1800
-ser.write("0,G,100\r\n".encode())
-ser.write("1,G,100\r\n".encode())
+while True:
 
-# Read Position
-ser.write("0,P\r\n".encode())
-print("Max speed of Motor 0 (after turning):", read())
-ser.write("1,P\r\n".encode())
-print("Max speed of Motor 1 (after turning):", read())
+    # Read Position
+    ser.write("0,P\r\n".encode())
+    print("Position of Motor 0 :", read())
+    ser.write("1,P\r\n".encode())
+    print("Position of Motor 1 :", read())
 
-ser.close()  # close port
+    time.sleep(0.1)
+
+    # Go To 1800 steps
+    ser.write("0,G,500\r\n".encode())
+    ser.write("1,G,500\r\n".encode())
+
+    time.sleep(3)
+
+    # Read Position
+    ser.write("0,P\r\n".encode())
+    print("Position of Motor 0 :", read())
+    ser.write("1,P\r\n".encode())
+    print("Position of Motor 1 :", read())
+
+    time.sleep(0.1)
+
+    # Go To 0 steps
+    ser.write("0,G,0\r\n".encode())
+    ser.write("1,G,0\r\n".encode())
+
+    time.sleep(3)
+
+    print("Next Loop")
--- Serial.py
+++ Serial.py
@@ -11,14 +11,26 @@ def read():
     return ser.readline().decode()
 
 
-# Go To 1800
-ser.write("0,G,100\r\n".encode())
-ser.write("1,G,100\r\n".encode())
+# Max Speed to 250
+ser.write("0,M,250\r\n".encode())
+ser.write("1,M,250\r\n".encode())
 
 # Read Position
 ser.write("0,P\r\n".encode())
-print("Max speed of Motor 0 :", read())
+print("Position of Motor 0 (before turning):", read())
 ser.write("1,P\r\n".encode())
-print("Max speed of Motor 1 :", read())
+print("Position of Motor 1 (before turning):", read())
+
+# Go To 1800
+ser.write("0,G,0\r\n".encode())
+ser.write("1,G,0\r\n".encode())
+
+time.sleep(10)
+
+# Read Position
+ser.write("0,P\r\n".encode())
+print("Position of Motor 0 (after turning):", read())
+ser.write("1,P\r\n".encode())
+print("Position of Motor 1 (after turning):", read())
 
 ser.close()  # close port
